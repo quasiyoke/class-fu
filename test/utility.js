8be72597eabde0ba3850dbfdc7d4623f5a9c484d
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-29 03:24:27 +0400

    ``getClasses`` tested with classes-free elements.

--- test/utility.js
+++ test/utility.js
@@ -4,6 +4,11 @@ ClassFu(function(){
 
 	test('get classes', function(){
 		var el = document.createElement('b');
+		deepEqual($.getClasses(el), {}, 'Generates empty object on missed `class` attribute');
+		el.setAttribute('class', '');
+		deepEqual($.getClasses(el), {}, 'Generates empty object on empty `class` attribute');
+		el.setAttribute('class', '   ');
+		deepEqual($.getClasses(el), {}, 'Generates empty object on spacey `class` attribute');
 		el.setAttribute('class', 'a b  class foo-bar ');
 		deepEqual($.getClasses(el), {a: null, b: null, class: null, 'foo-bar': null}, 'Gets CSS classes of element');
 	});
