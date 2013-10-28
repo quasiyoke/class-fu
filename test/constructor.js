d127eb7d9df9a433007ecdbc6e1fc2ad60ebf357
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-28 06:19:59 +0400

    "Constructor takes element" test added.

--- test/constructor.js
+++ test/constructor.js
@@ -3,10 +3,17 @@ module('Constructor');
 
 ClassFu(function(){
 	test('dollar sign', function(){
-		equal($, ClassFu, 'ClassFu could be accesed through $ shortcut');
+		equal($, ClassFu, 'ClassFu could be accessed through $ shortcut');
 	});
 	
 	test('previous dollar sign', function(){
 		equal($.previousDollarSign, dollarSign, 'ClassFu.previousDollarSign value is ok');
+	});
+
+	test('takes element', function(){
+		var el = document.createElement('b');
+		var $el = $(el);
+		equal($el[0], el, 'Element can be accessed through subscript');
+		equal($el.length, 1, 'instance.length === 1');
 	});
 });
