728744a835371356f06ffc57734c9b0c24af6df0
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-28 06:25:48 +0400

    "Constructor takes HTMLCollection" test added.

--- test/constructor.js
+++ test/constructor.js
@@ -16,4 +16,14 @@ ClassFu(function(){
 		equal($el[0], el, 'Element can be accessed through subscript');
 		equal($el.length, 1, 'instance.length === 1');
 	});
+
+	test('takes HTMLCollection', function(){
+		var els = document.getElementsByClassName('a');
+		var $els = $(els);
+		equal($els[0], els[0], 'Element 0 can be accessed through subscript');
+		equal($els[1], els[1], 'Element 1 can be accessed through subscript');
+		equal($els[2], els[2], 'Element 2 can be accessed through subscript');
+		equal($els[3], els[3], 'Element 3 can be accessed through subscript');
+		equal($els.length, 4, 'instance.length === 4');
+	});
 });
