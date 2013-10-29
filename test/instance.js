c606e1142981e8ce03078337431af47849a379b9
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-29 06:16:16 +0400

    Tests for command processing was added.

--- test/instance.js
+++ test/instance.js
@@ -13,6 +13,20 @@ ClassFu(function(){
 		deepEqual($els.classes(), {alpha: null, beta: null}, 'Gets classes of first element');
 	});
 
+	test('process command', function(){
+		var els = [];
+		els.push(document.createElement('b'));
+		els.push(document.createElement('b'));
+		els[1].setAttribute('class', 'alpha delta epsilon');
+		els.push(document.createElement('b'));
+		els[2].setAttribute('class', 'beta gamma zeta');
+		var $els = $(els);
+		equal($els.classes('- alpha - beta + gamma + delta !epsilon !zeta'), $els, 'Returns self');
+		deepEqual($.getClasses(els[0]), {gamma: null, delta: null, epsilon: null, zeta: null}, 'Element 0 `class` attribute correct');
+		deepEqual($.getClasses(els[1]), {gamma: null, delta: null, zeta: null}, 'Element 1 `class` attribute correct');
+		deepEqual($.getClasses(els[2]), {gamma: null, delta: null, epsilon: null}, 'Element 2 `class` attribute correct');
+	});
+
 	test('set classes', function(){
 		var $els = $();
 		equal($els.classes('alpha beta'), $els, 'Returns self when empty');
